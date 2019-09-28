c01cef8fa96bbb29b91307a5d8aa50a061e99cc9
Science001 <science.ganesh007@gmail.com>
2019-09-28 12:58:58 +0530

    Get sensor data

--- api.js
+++ api.js
@@ -30,4 +30,36 @@ api.get('/', (req, res) => {
     res.send({ message: "Team 13, FTW!" })
 })
 
+api.get('/sensor-data', (req, res) => {
+    pool.query("select * from sensor_data", (err, result) => {
+        if (err) {
+            console.log("Error getting sensor data: ", err)
+            res.status(500).send(err)
+        }
+        else {
+            res.send(result.rows)
+        }
+    })
+})
+
+api.post('/sensor-data', (req, res, next) => {
+    var { temperature, humidity, hcho } = req.body
+    if(!temperature || !humidity || !hcho) {
+        res.status(400).send("Not all expected data was sent")
+    }
+    else next()
+}, (req, res) => {
+    console.log("SENSOR DATA:", req.body)
+    var { temperature, humidity, hcho } = req.body
+    pool.query("insert into sensor_data(hcho, temperature, humidity) values ($1, $2, $3)", [hcho, temperature, humidity], (err, _) => {
+        if (err) {
+            console.log("Error inserting sensor data: ", err)
+            res.status(500).send(err)
+        }
+        else {
+            res.send("INSERTED INTO DB")
+        }
+    })
+})
+
 module.exports = api;
